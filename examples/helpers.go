8168919f561d6c0f8dd339b46058c966fa150c96
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-04-26 11:52:18 +0200

    Use the new attr package in example

--- examples/helpers.go
+++ examples/helpers.go
@@ -2,14 +2,15 @@ package examples
 
 import (
 	"github.com/bep/gr"
+	"github.com/bep/gr/attr"
 	"github.com/bep/gr/el"
 )
 
 // Alert creates a Bootstrap alert element.
 func Alert(classifier string, body gr.Modifier) *gr.Element {
 	e := el.Div(
-		gr.CSS("alert", "alert-success"),
-		el.Anchor(gr.Prop("href", "#"),
+		gr.CSS("alert", "alert-"+classifier),
+		el.Anchor(attr.HRef("#"),
 			gr.CSS("close"), gr.Data("dismiss", "alert"), gr.Aria("label", "close"),
 			gr.Text("Close")),
 		body)
