10d9aaa21785bb2c79949c30dad5c65600dc174a
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-04-29 11:28:18 +0200

    Add plain JS React component to interop example
    Looks like the render before componentDidMount is happening in plain JavaScript, too.
    
    Weird.
    
    See #18

--- examples/interop/interop-ext-reverse.js
+++ examples/interop/interop-ext-reverse.js
@@ -1,12 +1,29 @@
-
 var start = new Date().getTime()
 
 setInterval(function () {
     ReactDOM.render(
-	    Elapser({
-            elapsed: Math.round((( new Date().getTime() - start ) / 1000)),
-			title: "Interop from JavaScript"
+        Elapser({
+            elapsed: Math.round(((new Date().getTime() - start) / 1000)),
+            title: "Interop from JavaScript"
         }),
         document.getElementById('react-reverse')
     );
 }, 50);
+
+
+var Hello = React.createClass({
+    render: function () {
+        console.log("JS: render")
+        return React.DOM.h1(null, "JS Render: Go React!")
+    },
+
+    componentDidMount: function () {
+        console.log("JS: componentDidMount")
+    }
+});
+
+
+ReactDOM.render(
+    React.createElement(Hello),
+    document.getElementById('react-hello')
+);
